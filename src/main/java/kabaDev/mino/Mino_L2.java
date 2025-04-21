3dd2d9781e83025db89f12a100f268e7a264cedb
kabal <kabalera@gmail.com>
2025-04-21 23:12:24 +0200

    X

--- src/main/java/kabaDev/mino/Mino_L2.java
+++ src/main/java/kabaDev/mino/Mino_L2.java
@@ -8,38 +8,40 @@ public class Mino_L2 extends Mino {
         super.create(Color.blue);
     }
 
-    //Constructor del mino l2
-
-    public void setXY(int x, int y){
-
+    @Override
+    public void setXY(int x, int y) {
+        // Esquema inicial:
+        //   o
+        //   o BO
+        // o o
         b[0].x = x;
         b[0].y = y;
         b[1].x = b[0].x;
-        b[1].y = b[0].y - Block.SIZE;
+        b[1].y = b[0].y + Block.SIZE;
         b[2].x = b[0].x;
-        b[2].y = b[0].y + Block.SIZE;
+        b[2].y = b[0].y - Block.SIZE;
         b[3].x = b[0].x - Block.SIZE;
-        b[3].y = b[0].y + Block.SIZE;
+        b[3].y = b[0].y - Block.SIZE;
     }
 
     @Override
     public void getDirection1() {
         // Dirección 1: Vertical
         //    o        b3
-        //    o        b2
-        //  o o     b1 b0
+        //    o        b0
+        //  o o     b2 b1
         tempB[0].x = b[0].x;
         tempB[0].y = b[0].y;
-        tempB[1].x = b[0].x - Block.SIZE; // Se desplaza en el eje X negativo
-        tempB[1].y = b[0].y;
+        tempB[1].x = b[0].x;
+        tempB[1].y = b[0].y + Block.SIZE;
         tempB[2].x = b[0].x;
-        tempB[2].y = b[0].y + Block.SIZE;
+        tempB[2].y = b[0].y - Block.SIZE;
         tempB[3].x = b[0].x - Block.SIZE;
-        tempB[3].y = b[0].y + Block.SIZE;
-    
+        tempB[3].y = b[0].y - Block.SIZE;
+
         super.updateXY(1);
     }
-    
+
     @Override
     public void getDirection2() {
         // Dirección 2: Horizontal
@@ -49,15 +51,15 @@ public class Mino_L2 extends Mino {
         tempB[0].x = b[0].x;
         tempB[0].y = b[0].y;
         tempB[1].x = b[0].x - Block.SIZE;
-        tempB[1].y = b[0].y + Block.SIZE;
-        tempB[2].x = b[0].x - Block.SIZE;
-        tempB[2].y = b[0].y + Block.SIZE;
+        tempB[1].y = b[0].y;
+        tempB[2].x = b[0].x + Block.SIZE;
+        tempB[2].y = b[0].y;
         tempB[3].x = b[0].x + Block.SIZE;
-        tempB[3].y = b[0].y + Block.SIZE;
-    
+        tempB[3].y = b[0].y - Block.SIZE;
+
         super.updateXY(2);
     }
-    
+
     @Override
     public void getDirection3() {
         // Dirección 3: Vertical invertida
@@ -67,21 +69,21 @@ public class Mino_L2 extends Mino {
         tempB[0].x = b[0].x;
         tempB[0].y = b[0].y;
         tempB[1].x = b[0].x;
-        tempB[1].y = b[0].y + Block.SIZE;
+        tempB[1].y = b[0].y - Block.SIZE;
         tempB[2].x = b[0].x;
-        tempB[2].y = b[0].y - Block.SIZE;
+        tempB[2].y = b[0].y + Block.SIZE;
         tempB[3].x = b[0].x + Block.SIZE;
-        tempB[3].y = b[0].y - Block.SIZE;
-    
+        tempB[3].y = b[0].y + Block.SIZE;
+
         super.updateXY(3);
     }
-    
+
     @Override
     public void getDirection4() {
         // Dirección 4: Horizontal invertida
-        //     o
-        // o o o
         //
+        // o o o
+        //     o
         tempB[0].x = b[0].x;
         tempB[0].y = b[0].y;
         tempB[1].x = b[0].x + Block.SIZE;
@@ -90,10 +92,7 @@ public class Mino_L2 extends Mino {
         tempB[2].y = b[0].y;
         tempB[3].x = b[0].x - Block.SIZE;
         tempB[3].y = b[0].y + Block.SIZE;
-    
+
         super.updateXY(4);
     }
-
-    
-    
 }
--- src/main/java/kabaDev/mino/Mino_Z2.java
+++ src/main/java/kabaDev/mino/Mino_Z2.java
@@ -47,13 +47,13 @@ public class Mino_Z2 extends Mino {
         //        o
         //     o  o
         //     o
-        tempB[0].x = b[0].x; // b[0] es el bloque central en la fila del medio
+        tempB[0].x = b[0].x;
         tempB[0].y = b[0].y;
-        tempB[1].x = b[0].x; // b[1] está debajo de b[0]
+        tempB[1].x = b[0].x;
         tempB[1].y = b[0].y + Block.SIZE;
-        tempB[2].x = b[0].x - Block.SIZE; // b[2] está a la izquierda de b[0]
+        tempB[2].x = b[0].x + Block.SIZE;
         tempB[2].y = b[0].y;
-        tempB[3].x = b[0].x + Block.SIZE; // b[3] está arriba a la derecha de b[0]
+        tempB[3].x = b[0].x + Block.SIZE;
         tempB[3].y = b[0].y - Block.SIZE;
     
         super.updateXY(2);
